26b6c6580c1991d6004e81550fc553fc45367686
Hoang Hy <hoanghy01122003@gmail.com>
2023-12-13 02:58:59 +0700

    add utils

---
+++ src/constants/searchParams.ts
@@ -0,0 +1,10 @@
+const SEARCH_PARAMS = {
+    redirectUri: "redirect_uri",
+    productName: "product_name",
+    categoryName: "category_name",
+    price: "price",
+    name: "name",
+};
+
+export default SEARCH_PARAMS;
+
